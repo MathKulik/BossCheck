68da08e8393086853e8d5bda05f3af6317c2c4dc
Math.Kulik <matheuskulikk@gmail.com>
2023-10-27 18:29:31 -0300

    a college project based on Terraria's Boss Check List mod

---
+++ app/src/main/java/com/terraria/bosscheck/MainActivity.java
@@ -0,0 +1,24 @@
+package com.terraria.bosscheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void corruption( View view ) {
+        Intent intent = new Intent(this, preHardC.class);
+        startActivity(intent);
+    }
+    public void crimson(View view){
+        Intent intent = new Intent(this, preHardCa.class);
+        startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/com/terraria/bosscheck/preHardC.java
@@ -0,0 +1,14 @@
+package com.terraria.bosscheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class preHardC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_hard_c);
+    }
+}
